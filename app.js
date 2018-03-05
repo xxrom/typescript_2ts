f589b2cc1597b3086f68c44b730ebdc7dd0588f8
Nikita Chernyshov <xpom55@mail.ru>
2018-03-05 09:02:10 +0300

    tsconfig simple examples

---
+++ app.js
@@ -0,0 +1,13 @@
+"use strict";
+var myName = "Max";
+var myAge = 27;
+var anything;
+anything = 12;
+function controlMe(isTrue, somethingElse) {
+    var result;
+    if (isTrue) {
+        result = 12;
+    }
+    result = 33;
+    return result;
+}
---
+++ app.ts
@@ -0,0 +1,14 @@
+let myName: string = "Max";
+let myAge: number = 27;
+
+let anything;
+anything = 12;
+
+function controlMe(isTrue: boolean, somethingElse: boolean) {
+  let result: number;
+  if (isTrue) {
+    result = 12;
+  }
+  result = 33;
+  return result;
+}
