6113ccfbf94db3c3c1d181de3b8763ed255138bb
Nikita Chernyshov <xpom55@mail.ru>
2018-03-27 15:57:11 +0300

    init commit

--- app.js
+++ app.js
@@ -10,5 +10,7 @@ var MyMath;
     }
     MyMath.calculateRectangle = calculateRectangle;
 })(MyMath || (MyMath = {}));
+var PI = 2.99;
 console.log(MyMath.calculateRectangle(10, 20));
 console.log(MyMath.calculateCircumference(3));
+console.log(PI);
--- app.ts
+++ app.ts
@@ -9,6 +9,14 @@ namespace MyMath {
     return width * length;
   }
 }
+const PI = 2.99;
 
 console.log(MyMath.calculateRectangle(10, 20));
 console.log(MyMath.calculateCircumference(3));
+console.log(PI);
+
+
+
+
+
+
